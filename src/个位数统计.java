55a92cd06c3f95293009260f4b697342414e9537
821453366 <821453366@qq.com>
2017-03-12 17:33:27 +0800

    出租 和 个数统计提交

---
+++ src/个位数统计.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class 个位数统计 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String arr = input.next();
+        int[] n = new int[10];
+
+        for (int i=0;i<arr.length();i++){
+            n [arr.charAt(i)-'0']++;
+        }
+        for (int i=0; i<n.length;i++){
+            if(n[i]!=0){
+                System.out.println(i+":"+n[i]);
+            }
+        }
+
+    }
+}
+
+/*
+
+import java.util.Scanner;
+public class 个位数统计 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a = new int[10];
+        String n = in.next();
+        for(int i = 0;i<n.length();i++)
+        a[n.charAt(i)-'0']++;
+        for (int arr:a) {
+            System.out.println(arr);
+
+        }
+       for(int i = 0;i<a.length;i++)
+        if(a[i]!=0)
+        System.out.println(i+":"+a[i]);
+    }
+}
+*/
+
+
---
+++ src/出租.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Created by pc on 17-3-9.
+ */
+public class 出租 {
+    public static void main (String[] agrs){
+        String arr="";
+        int b=0;
+        int n[]=new int [10];
+        int aa[]=new int [11];
+        Scanner input = new Scanner(System.in);
+        String s_num=input.next();
+        for(int i= 0 ;i<s_num.length();i++){
+            n[s_num.charAt(i)-'0']++;
+        }
+        System.out.print("int[] arr = new int[]{");
+        for (int i= n.length-1;i>=0;i--){
+            if(n[i]!=0){
+                arr+=i;
+                b++;
+                if (b == 1) {
+                    System.out.print(i);
+                }else {
+                    System.out.print(","+i);
+                }
+            }
+        }
+        System.out.println("};");
+
+        System.out.print("int[] index = new int[]{");
+        b=0;
+        for(int i = 0; i<aa.length;i++){
+            b++;
+           aa[i]= arr.indexOf(s_num.charAt(i));
+           if(b==1){
+            System.out.print(aa[i]);
+           }else {
+               System.out.print(","+aa[i]);
+           }
+        }
+        System.out.print("};");
+
+
+
+    }
+}
