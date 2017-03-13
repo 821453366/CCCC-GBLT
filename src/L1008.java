d785ad11be60c92bee854bd7ca9047261f9a99a2
821453366 <821453366@qq.com>
2017-03-13 15:32:02 +0800

    提交A-B到github

---
+++ src/L1008.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**求整数段和
+ * Created by pc on 17-3-12.
+ */
+public class L1008 {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        int i_begin = input .nextInt();
+        int i_end = input.nextInt();
+        int a=0;
+        int sum=0;
+        for(int i=i_begin ;i<=i_end;i++){
+
+            if(a==5){
+                a=0;
+                System.out.println();
+            }
+            sum +=i;
+            System.out.printf("%5d",i);
+
+            a++;
+        }
+        System.out.println();
+        System.out.print("Sum = "+sum);
+
+    }
+}
---
+++ src/L1_011.java
@@ -0,0 +1,6 @@
+/**A-B
+ * Created by pc on 17-3-13.
+ */
+public class L1_011 {
+
+}
